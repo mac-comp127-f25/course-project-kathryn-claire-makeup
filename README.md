d0c71ec6f13aae749b9f19d39e7cafbc8726534a
Claire Kuno <ckuno@macalester.edu>
2025-11-19 10:21:20 -0600

    checking canvas and readme

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Project Title Goes Here
+# Makeup
 
-Info about your project goes here
+## A title 
+Makeup Painter
+
+## Your group’s members
+Kathryn Totushek and Claire Kuno
+
+## A brief description (think of a single sentence / catchphrase)
+A makeup visualizing tool that allows the user to choose shades of eye, lip, and face makeup. 
+
+## Drawings, if appropriate – could be the UI, or a high-level diagram of the workflow / user interaction, etc.
+Face.java (contains the graphics for the shape of the face, eyes, nose, lips, etc.)
+Color.java (add for the more complex coloring by creating bounds for the application of blurred shades on the canvas)
+MakeupPainter.java (creates the main canvas)
+Makeup.java (the different types of makeup)
+	Eyeshadow.java
+	Blush.java
+	Lipstick.java
+	Lashes.java
+
+## Tell us why your project is cool, interesting, useful, and so on!
+Our project allows the user to have full creative reign on creating makeup on a face however they want to. 
+
+## Describe a smaller, initial version of your project that you can build first. This will be a prototype, or proof of concept, or “minimum viable product”. Something with a scope smaller than the full project.
+Instead of drawing the makeup, the user will be able to just click on an area of the face and the area will automatically get colored in. 
+
+## You should have shared your project idea with students outside your group. What did they say?
+
+
--- src/makeup/MakeupApp.java
+++ src/makeup/MakeupApp.java
@@ -3,7 +3,7 @@ package makeup;
 import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.Point;
-import makeup.SprayPaint.Brush;
+// import makeup.SprayPaint.Brush;
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -14,7 +14,7 @@ import edu.macalester.graphics.ui.Button;
 public class MakeupApp {
     
     private CanvasWindow canvas;
-    private final BrushSettingsView brushSettingsView;
+    // private final BrushSettingsView brushSettingsView;
     private Brushes currentBrush;
     private GraphicsGroup paintLayer; 
 
@@ -23,43 +23,42 @@ public class MakeupApp {
     public MakeupApp() {
         canvas = new CanvasWindow("Makeup", 900, 800);
 
+    //     brushSettingsView = new BrushSettingsView(Color.BLUE, 60);
+    //     canvas.add(brushSettingsView, 10 - brushSettingsView.getBounds().getMinX(), 10);
 
-        brushSettingsView = new BrushSettingsView(Color.BLUE, 60);
-        canvas.add(brushSettingsView, 10 - brushSettingsView.getBounds().getMinX(), 10);
+    //     currentBrush = new Brush.CirclesBrush();
 
-        currentBrush = new Brush.CirclesBrush();
+    //     canvas.onMouseDown(event -> sprayPaint(event.getPosition()));
+    //     canvas.onDrag(event -> sprayPaint(event.getPosition()));
 
-        canvas.onMouseDown(event -> sprayPaint(event.getPosition()));
-        canvas.onDrag(event -> sprayPaint(event.getPosition()));
+    //     availableBrushes.add(new Brush.SprayBrush());
+    //     availableBrushes.add(new Brush.Eraser());
 
-        availableBrushes.add(new Brush.SprayBrush());
-        availableBrushes.add(new Brush.Eraser());
+    //     double y = 300;
+    //     for (Brushes brush : availableBrushes) {
+    //         addBrushButton(brush, y);
+    //         y += 40;
+    //     }
 
-        double y = 300;
-        for (Brushes brush : availableBrushes) {
-            addBrushButton(brush, y);
-            y += 40;
-        }
+    // }
 
-    }
+    // public void sprayPaint(Point location) {
+    //     BrushOptions brushOptions = brushSettingsView.getBrushOptions();
+    //     Color color = brushOptions.getColor();
+    //     float radius = brushOptions.getRadius();
+    //     this.currentBrush.apply(paintLayer, location, brushOptions, color, radius);
 
-    public void sprayPaint(Point location) {
-        BrushOptions brushOptions = brushSettingsView.getBrushOptions();
-        Color color = brushOptions.getColor();
-        float radius = brushOptions.getRadius();
-        this.currentBrush.apply(paintLayer, location, brushOptions, color, radius);
+    // }
 
-    }
+    // public void addBrushButton(Brushes brush, double y) {
+    //     String label = brush.getClass().getSimpleName();
+    //     Button button = new Button(label);
+    //     button.setPosition(10, y);
+    //     canvas.add(button);
 
-    public void addBrushButton(Brushes brush, double y) {
-        String label = brush.getClass().getSimpleName();
-        Button button = new Button(label);
-        button.setPosition(10, y);
-        canvas.add(button);
-
-        button.onClick(() -> {
-            currentBrush = brush;
-        });
+    //     button.onClick(() -> {
+    //         currentBrush = brush;
+    //     });
     }
 
     public static void main(String[] args) {
